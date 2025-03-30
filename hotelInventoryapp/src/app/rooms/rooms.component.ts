a80ccd294b113274bb7cf10135947bc17f07f511
vishwajeet161 <vishwajeet.patel161@gmail.com>
2025-03-30 23:52:31 +0530

    Used servies to get the data

--- hotelInventoryapp/src/app/rooms/rooms.component.ts
+++ hotelInventoryapp/src/app/rooms/rooms.component.ts
@@ -58,38 +58,7 @@ export class RoomsComponent implements OnInit, DoCheck, AfterViewInit, AfterView
   */
   ngOnInit() {
     // console.log(this.headerComponent);
-    this.roomList = [{
-      roomNumber: 101,
-      roomType: 'Deluxe Room',
-      amenities: 'AC, TV, Wifi, Breakfast',
-      price: 5000,
-      photos: 'assets/deluxe.jpg',
-      checkinTime: new Date('2021-09-01'),
-      checkoutTime: new Date('2021-09-10'),
-      rating: 3.4678
-    },
-    {
-      roomNumber: 102,
-      roomType: 'Super Deluxe Room',
-      amenities: 'AC, TV, Wifi, Breakfast, Lunch, Dinner',
-      price: 10000,
-      photos: 'assets/superdeluxe.jpg',
-      checkinTime: new Date('2021-09-01'),
-      checkoutTime: new Date('2021-09-10'),
-      rating: 4.2
-    },
-    {
-      roomNumber: 103,
-      roomType: 'Suite Room',
-      amenities: 'AC, TV, Wifi, Breakfast, Lunch, Dinner, Bar',
-      price: 15000,
-      photos: 'assets/suite.jpg',
-      checkinTime: new Date('2021-09-01'),
-      checkoutTime: new Date('2021-09-10'),
-      rating: 4.811
-    }
-
-    ]
+   
   }
 
  
--- hotelInventoryapp/src/app/rooms/services/rooms.service.ts
+++ hotelInventoryapp/src/app/rooms/services/rooms.service.ts
@@ -1,9 +1,47 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
+import { RoomList } from '../rooms';
 
 @Injectable({
   providedIn: 'root'
 })
-export class RoomsService {
+export class RoomsService  {
 
   constructor() { }
+
+  roomList: RoomList[] = [{
+    roomNumber: 101,
+    roomType: 'Deluxe Room',
+    amenities: 'AC, TV, Wifi, Breakfast',
+    price: 5000,
+    photos: 'assets/deluxe.jpg',
+    checkinTime: new Date('2021-09-01'),
+    checkoutTime: new Date('2021-09-10'),
+    rating: 3.4678
+  },
+  {
+    roomNumber: 102,
+    roomType: 'Super Deluxe Room',
+    amenities: 'AC, TV, Wifi, Breakfast, Lunch, Dinner',
+    price: 10000,
+    photos: 'assets/superdeluxe.jpg',
+    checkinTime: new Date('2021-09-01'),
+    checkoutTime: new Date('2021-09-10'),
+    rating: 4.2
+  },
+  {
+    roomNumber: 103,
+    roomType: 'Suite Room',
+    amenities: 'AC, TV, Wifi, Breakfast, Lunch, Dinner, Bar',
+    price: 15000,
+    photos: 'assets/suite.jpg',
+    checkinTime: new Date('2021-09-01'),
+    checkoutTime: new Date('2021-09-10'),
+    rating: 4.811
+  }
+
+  ]
+
+  getRooms(){
+    return this.roomList;
+  }
 }
